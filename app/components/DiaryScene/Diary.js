0fca6639d50643dbbce482d6cf92fa2e067c3ea8
Andrew Lee <andyrewlee@gmail.com>
2017-03-01 20:02:40 -0800

    Refactory out to Today component

--- app/components/DiaryScene/Diary.js
+++ app/components/DiaryScene/Diary.js
@@ -2,86 +2,67 @@ import React, { Component } from 'react';
 import Icon from 'react-native-vector-icons/Entypo';
 
 import {
-  StyleSheet,
-  TabBarIOS,
-  Text,
   View,
-  TextInput,
+  Text,
+  TabBarIOS,
+  StyleSheet,
   KeyboardAvoidingView
 } from 'react-native';
+
+import Today from './Today';
 
 export default class Diary extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      selectedTab: 'todayTab',
-      text: ''
-    };
-  }
-
-  closeKeyboard() {
-    Alert.alert('hi', 'ho');
+    this.state = { selectedTab: 'todayTab' };
   }
 
   renderContent(pageText) {
     return (
       <View style={styles.tabContent}>
-        <TextInput
-          style={{height: 400, borderColor: 'transparent'}}
-          onChangeText={(text) => this.setState({text})}
-          placeholder="how was your day?"
-          padding={16}
-          multiline
-          value={this.state.text}
-        />
+        <Today />
       </View>
     );
   }
+
   render() {
     return (
       <KeyboardAvoidingView behavior="padding" style={styles.container}>
-      <TabBarIOS
-        unselectedTintColor="#00585b"
-        tintColor="#099499"
-        unselectedItemTintColor="#00585b"
-        barTintColor="#d0ad8f">
-        <Icon.TabBarItemIOS
-          title="Diary"
-          iconName="stopwatch"
-          selected={this.state.selectedTab === 'diaryTab'}
-          onPress={() => {
-            this.setState({
-              selectedTab: 'diaryTab'
-            });
-          }}>
-          {this.renderContent('Diary')}
-        </Icon.TabBarItemIOS>
-        <Icon.TabBarItemIOS
-          title="Today"
-          iconName="pencil"
-          selected={this.state.selectedTab === 'todayTab'}
-          onPress={() => {
-            this.setState({
-              selectedTab: 'todayTab'
-            });
-          }}>
-          {this.renderContent('Today')}
-        </Icon.TabBarItemIOS>
-        <Icon.TabBarItemIOS
-          title="Diaries"
-          iconName="light-up"
-          title="Diaries"
-          selected={this.state.selectedTab === 'diariesTab'}
-          onPress={() => {
-            this.setState({
-              selectedTab: 'diariesTab'
-            });
-          }}>
-          {this.renderContent('Diaries')}
-        </Icon.TabBarItemIOS>
-      </TabBarIOS>
-
-        </KeyboardAvoidingView>
+        <TabBarIOS
+          unselectedTintColor="#00585b"
+          tintColor="#099499"
+          unselectedItemTintColor="#00585b"
+          barTintColor="#d0ad8f">
+          <Icon.TabBarItemIOS
+            title="Diary"
+            iconName="stopwatch"
+            selected={this.state.selectedTab === 'diaryTab'}
+            onPress={() => {
+              this.setState({ selectedTab: 'diaryTab' });
+            }}>
+            {this.renderContent('Diary')}
+          </Icon.TabBarItemIOS>
+          <Icon.TabBarItemIOS
+            title="Today"
+            iconName="pencil"
+            selected={this.state.selectedTab === 'todayTab'}
+            onPress={() => {
+              this.setState({ selectedTab: 'todayTab' });
+            }}>
+            {this.renderContent('Today')}
+          </Icon.TabBarItemIOS>
+          <Icon.TabBarItemIOS
+            title="Diaries"
+            iconName="light-up"
+            title="Diaries"
+            selected={this.state.selectedTab === 'diariesTab'}
+            onPress={() => {
+              this.setState({ selectedTab: 'diariesTab' });
+            }}>
+            {this.renderContent('Diaries')}
+          </Icon.TabBarItemIOS>
+        </TabBarIOS>
+      </KeyboardAvoidingView>
     );
   }
 }
@@ -92,10 +73,6 @@ const styles = StyleSheet.create({
     paddingTop: 80,
     alignItems: 'center',
     backgroundColor: '#dfc7b2'
-  },
-  tabText: {
-    color: 'white',
-    marginTop: 30
   },
   container: {
     flex: 1,
--- app/components/DiaryScene/Today.js
+++ app/components/DiaryScene/Today.js
@@ -1,4 +1,34 @@
 import React, { Component } from 'react';
 
+import {
+  TextInput,
+  StyleSheet
+} from 'react-native';
+
 export default class Today extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+
+  render() {
+    return (
+      <TextInput
+        style={styles.textInput}
+        onChangeText={(text) => this.setState({text})}
+        placeholder="how was your day?"
+        multiline
+        value={this.state.text}
+      />
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  textInput: {
+    height: 400,
+    borderColor: 'transparent',
+    paddingRight: 16,
+    paddingLeft: 16
+  }
+});
